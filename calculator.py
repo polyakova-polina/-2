dc795a8fd6d54608671de4c022c2fb1987d4ba2d
admin <pnp.home@mail.ru>
2024-02-01 16:37:36 +0300

    испр

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ def m(a ,b, c, d):
 
 z = np.array([[1,0,0]]).T
 e = np.array([[0,1,0]]).T
-f = np.array([0,0,1]).T
+f = np.array([[0,0,1]]).T
 A = [z, e, f]
 
 B = []
@@ -28,11 +28,15 @@ for i1 in range(3):
             for i4 in range(3):
                 B.append(m(A[i1], A[i2], A[i3], A[i4]))
 
-
 ro = np.eye(3**5)
 tr = np.eye(3) - np.eye(3)
+tr1 = 0
+
 for i in range(3):
     for j in range(3):
         for k in range(81):
-            tr = tr + np.kron(A[i].T, B[k].T) @ ro @ np.kron(A[j], B[k]) * A[i] @ A[j].T
+            tr1 = 0
+            tr1 = tr1 + np.kron(A[i].T, B[k].T) @ ro @ np.kron(A[j], B[k])
+        tr = tr + tr1 * A[i] @ A[j].T
+
 print(tr)
