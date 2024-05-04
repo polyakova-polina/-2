a82ffabc68411aa2edb0d78c9dead08620af5884
maslakov <ivamaslakov@gmail.com>
2024-05-05 00:35:32 +0300

    ccx_Polina

--- CCX.py
+++ CCX.py
@@ -90,20 +90,54 @@ u4 = np.kron(R(np.pi / 2, -np.pi / 2, 0, 1), I)
 u5 = np.kron(R(0, np.pi, 1, 2), I)
 xx01 = make_ms_matrix(3, 0, np.pi / 2,0,1,0,1)
 cx = u1 @ u2 @ xx01 @ u3 @ u4 @ u5
+
 u1 = np.kron(R(np.pi/2, np.pi / 2, 0, 1), I)
 u11 = np.kron(R(np.pi/2, -np.pi / 2, 0, 1), I)
 u3 = np.kron(R(0, -np.pi, 0, 1), R(0, -np.pi, 0, 1))
 xx01_ = make_ms_matrix(3, 0, np.pi / 2,0,1,0,1)
-cx_ = u1 @ xx01_ @ u3 @ u11
+
+c2x = u1 @ xx01_ @ u3 @ u11
+
+uu1 = np.kron(I, R(np.pi/2, np.pi / 2, 0, 2))
+uu11 = np.kron(I, R(np.pi/2, -np.pi / 2, 0, 2))
+uu3 = np.kron(R(0, -np.pi, 0, 1), R(0, -np.pi, 0, 2))
+xxu01_ = make_ms_matrix(3, 0, np.pi / 2,0,1,0,2)
+
+x1c = uu1 @ xxu01_ @ uu3 @ uu11
+
+uuu1 = np.kron(R(np.pi/2, np.pi / 2, 0, 2), I)
+uuu11 = np.kron(R(np.pi/2, -np.pi / 2, 0, 2), I)
+uuu3 = np.kron(R(0, -np.pi, 0, 2), R(0, -np.pi, 0, 1))
+xxuu01_ = make_ms_matrix(3, 0, np.pi / 2,0,2,0,1)
+
+c1x = uuu1 @ xxuu01_ @ uuu3 @ uuu11
 
 
 u1_ = np.kron(R(0, -np.pi, 1, 2), R(np.pi / 2, -np.pi, 0, 2))
 u2_ = np.kron(R(np.pi / 2, np.pi / 2, 0, 1), I)
 U1 = u1_ @ u2_ @ xx01
 
-ccx = np.kron(U1, I) @ np.kron(I, cx) @ np.kron(dag(U1), I)
+x12 = R(0, np.pi, 1, 2)
+x02 = R(0, np.pi, 0, 2)
 
-print(abs(comp_m(ccx)))
+ccx = np.kron(U1, I) @ np.kron(I, cx) @ np.kron(dag(U1), I)
+mod = np.kron(np.kron(R(0,np.pi,0,1) @ R(0,np.pi,1,2),R(0,np.pi,0,1) @ R(0,np.pi,1,2)), I)
+''' 
+swap12 = np.kron(np.array([
+ [1, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 1, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 1]]), I)
+ '''
+
+swap12 = np.kron(x1c@c1x@x1c@np.kron(x12,x12)@x1c@c1x@x1c@np.kron(x12,x12)@np.kron(x02,x02)@x1c@c1x@x1c@np.kron(x02,x02),I)
+
+print(np.trace(abs(comp_m(ccx @ swap12 @ mod @ ccx @ (np.kron(I,c2x)) @ mod @ mod @ swap12))))
 
 
 
